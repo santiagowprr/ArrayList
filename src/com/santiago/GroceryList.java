41062103aca73f20350edc62e78cc2890e059238
Santiagowprr <jk.wrob@gmail.com>
2021-01-25 12:27:43 +0100

    added GroceryList

---
+++ src/com/santiago/GroceryList.java
@@ -0,0 +1,5 @@
+package com.santiago;
+
+public class GroceryList {
+
+}
--- src/com/santiago/Main.java
+++ src/com/santiago/Main.java
@@ -3,6 +3,9 @@ package com.santiago;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // changing the length (size) of an array without creating a new one:
+        // Array list = resizeble array, handles the resizing automatically
+
+
     }
 }
